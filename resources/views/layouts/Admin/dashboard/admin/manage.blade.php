ed17378079964bef2b15e724d8018be5a4846462
Saung Eaindray Min <saungeaindraymin@gmail.com>
2025-01-22 20:12:43 +0630

    last update

--- resources/views/layouts/Admin/dashboard/admin/manage.blade.php
+++ resources/views/layouts/Admin/dashboard/admin/manage.blade.php
@@ -20,19 +20,6 @@
                     <input type="text" name="search" value="{{ $searchTerm }}" class="py-2 ps-10 pe-16 block w-full bg-white border-slate-200 rounded-lg text-sm focus:outline-none" placeholder="Search by name or email">
                 </form>
 
-                <button class="flex gap-x-2 bg-[#FFFFF0] p-2 rounded-lg shadow-lg justify-center items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" class="size-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                    </svg>    
-                    <p class="hidden lg:block">Filter</p>                  
-                </button>
-
-                <button class="flex gap-x-2 bg-[#FFFFF0] p-2 rounded-lg shadow-lg justify-center items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" class="size-5">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                    </svg>                        
-                    <p class="hidden lg:block">Export</p>                  
-                </button>
 
                 <a class="flex gap-x-2 bg-[#FFFFF0] p-2 rounded-lg shadow-lg text-sm justify-center items-center  ">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" class="size-5">

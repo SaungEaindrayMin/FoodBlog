0089fc0f66b6b8f2a4e3f559e8e46ac6e13d7bc5
Saung Eaindray Min <saungeaindraymin@gmail.com>
2025-01-23 13:55:34 +0630

    dashboard route fixed

--- resources/views/layouts/User/dashboard/dashboard.blade.php
+++ resources/views/layouts/User/dashboard/dashboard.blade.php
@@ -143,32 +143,32 @@
           </li>
 
           <li class="hs-accordion" id="users-accordion">
-            <button type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-yellow-400 focus:outline-none focus:bg-gray-100 " aria-expanded="true" aria-controls="users-accordion-child">
+            <a href="{{ route('profile.index') }}" type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-yellow-400 focus:outline-none focus:bg-gray-100 " aria-expanded="true" aria-controls="users-accordion-child">
               <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
               Community Cookbook
 
              
-            </button>
+            </a>
 
           </li>
 
           <li class="hs-accordion" id="account-accordion">
-            <button type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-yellow-400 focus:outline-none focus:bg-gray-100 " aria-expanded="true" aria-controls="account-accordion-child">
+            <a href="{{ route('user.cookbook') }}" type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-yellow-400 focus:outline-none focus:bg-gray-100 " aria-expanded="true" aria-controls="account-accordion-child">
               <svg class="shrink-0 mt-0.5 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/><path d="M10 15H6a4 4 0 0 0-4 4v2"/><path d="m21.7 16.4-.9-.3"/><path d="m15.2 13.9-.9-.3"/><path d="m16.6 18.7.3-.9"/><path d="m19.1 12.2.3-.9"/><path d="m19.6 18.7-.4-1"/><path d="m16.8 12.3-.4-1"/><path d="m14.3 16.6 1-.4"/><path d="m20.7 13.8 1-.4"/></svg>
               Culinary Resource
 
     
-            </button>
+            </a>
 
           </li>
 
           <li class="hs-accordion" id="projects-accordion">
-            <button type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-yellow-400 focus:outline-none focus:bg-gray-100 " aria-expanded="true" aria-controls="projects-accordion-child">
+            <a href="{{ route('user.education') }}" type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-yellow-400 focus:outline-none focus:bg-gray-100 " aria-expanded="true" aria-controls="projects-accordion-child">
               <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="14" x="2" y="7" rx="2" ry="2"/><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"/></svg>
               Educational Resource
 
 
-            </button>
+            </a>
 
 
           </li>
